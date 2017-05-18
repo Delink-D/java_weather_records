0e34a40737f35638b4871eac5f1f2c499f213d72
Delink-D <delinkdesigns@gmail.com>
2017-05-18 22:42:41 +0300

    working solution

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -20,18 +20,45 @@ public class App {
 		// while loop will help loop the entire records
 		// the loop also reads in the data line by line
 		while ((record = input.readLine()) != null){
+			String[] myValues = record.split(" ");
 
 			// if loop is at line 1 or 31 skip them and continue with the loop
-        	if (linenum == 0 || linenum == 31) {
+        	if (linenum == 0 || linenum == 32) {
         		linenum++;
         		continue;
         	}
         	linenum++;
 
-			String[] myValues = record.split(" ");
+			// converting the columns values into integers
+        	// get only the columns that are needed
+        	// remove special characters at the end of some integers
 
-			// System.out.println(myValues[2]);
-			System.out.println(record); // displays all records
+        	// System.out.println(record);
+        	// Converting the values into integers
+        	Integer max = Integer.valueOf(myValues[1].replace("*",""));
+        	Integer min = Integer.valueOf(myValues[2].replace("*",""));
+
+        	int sub = max-min; //subtract each minimum value per day from maximum value
+
+        	result.put(myValues[0], sub); // with our hashMap put in the key=column[0] which is the day and the subtract value as the value
 		}
+
+		// outside the loop
+        // get the maximum value in the HashMap
+        // store it in a variable of type int
+        int maxVal = Collections.max(result.values());
+
+        // geting a key from the maximum value
+        // Itarate trough hashmap 
+        for(Entry<String, Integer> entry : result.entrySet()){
+        	if (entry.getValue() == maxVal) {
+        		// print the key with maximum value plus the maximum value itself
+        		// result print = day maximum spread
+        		System.out.println(entry.getKey() + " " + maxVal);
+        	}
+        }
+
+        // a method to close the input process
+        input.close();
 	}
 }
